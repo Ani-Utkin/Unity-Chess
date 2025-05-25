4377eaa2a76c9759077f02e75dda01684f3c9ead
Anna Utkin <annau25@gmail.com>
2025-05-25 12:25:35 -0400

    create scripts folder and set up GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameManager: MonoBehaviour
+{
+    public void Start()
+    {
+        //Create board
+        //Instantiate board pieces
+        //Set player turn to White
+        int num = 5;
+    }
+}
